522ed8e0dd35f56c9be510c34cb14cf02bbed313
Eterna1_love <shadow-ek-alex@mail.ru>
2026-06-05 04:42:13 +0500

    chore: build + build dir

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import pkg from './package.json'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'docs',
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
